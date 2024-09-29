21e77d9cb3245b3db85b84f420b7c5610a9ae30b
fasill <fasilhawultie19@gmail.com>
2024-09-29 22:32:24 +0300

    bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import components from "./components/index";
 
 const App = () => {
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import assets from "../assets/index";
 import Card from "./Card";
 interface Data {
